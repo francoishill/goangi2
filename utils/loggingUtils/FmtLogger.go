7a2249380e58857bdb96f50fd6a72f68967268be
Francois Hill <francoishill11@gmail.com>
2015-01-16 17:23:06 +0200

    Files in package `utils/loggingUtils` should have package name `loggingUtils`.

--- utils/loggingUtils/FmtLogger.go
+++ utils/loggingUtils/FmtLogger.go
@@ -1,4 +1,4 @@
-package logger
+package loggingUtils
 
 import (
 	"fmt"
--- utils/loggingUtils/ILogger.go
+++ utils/loggingUtils/ILogger.go
@@ -1,4 +1,4 @@
-package logger
+package loggingUtils
 
 type ILogger interface {
 	Emergency(format string, v ...interface{})
