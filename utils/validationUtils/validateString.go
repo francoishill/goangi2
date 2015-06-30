e2c1db5ca8d339309d4298756a79e31d8c16d5de
Francois <francoishill11@gmail.com>
2015-06-30 10:13:27 +0200

    Added validation method CheckStringIsOneOf.

--- utils/validationUtils/validateString.go
+++ utils/validationUtils/validateString.go
@@ -1,6 +1,7 @@
 package validationUtils
 
 import (
+	"fmt"
 	"strings"
 
 	. "github.com/francoishill/goangi2/utils/errorUtils"
@@ -10,4 +11,29 @@ func CheckStringNotEmpty(strToCheck, errorIfStringEmpty string) {
 	if strings.Trim(strToCheck, " ") == "" {
 		PanicValidationError(errorIfStringEmpty)
 	}
+}
+
+func CheckStringIsOneOf(strIn string, oneOf []string, ignoreCase, doTrim bool, errorIfNotInList string) {
+	for _, o := range oneOf {
+		str := strIn
+		one := o
+
+		if doTrim {
+			str = strings.Trim(strIn, " \t")
+			one = strings.Trim(o, " \t")
+		}
+
+		var equal bool
+		if ignoreCase {
+			equal = strings.EqualFold(str, one)
+		} else {
+			equal = str == one
+		}
+
+		if equal {
+			return
+		}
+	}
+
+	PanicValidationError(errorIfNotInList + fmt.Sprintf(" (must be in list %+v)", oneOf))
 }
